1a9814177e976dd43909fdc4a55059756bbfa82d
Parag <paraglondhe123@gmail.com>
2023-08-06 19:04:33 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import pickle
+import re
+from nltk.corpus import stopwords
+
+maxlen =50
+estopwords=stopwords.words('english')
+estopwords.remove('not')
+TAG_RE = re.compile(r'<[^>]+>')
+def remove_tags(text):
+    return TAG_RE.sub('', text)
+def preprocess_text(sen, stopwords=estopwords, singleremover=True):
+    '''Cleans text data up, leaving only 2 or more char long non-stepwords composed of A-Z & a-z only
+    in lowercase'''
+    sentence = sen.lower()
+    # Remove html tags
+    sentence = remove_tags(sentence)
+    # Remove punctuations and numbers
+    sentence = re.sub('[^a-zA-Z]', ' ', sentence)
+    # Single character removal
+    if singleremover:
+        sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)
+
+    sentence = re.sub(r'\s+', ' ',sentence)
+    # Remove Stopwords
+    pattern = re.compile(r'\b(' + r'|'.join(stopwords) + r')\b\s*')
+    sentence = pattern.sub('', sentence)
+
+    return sentence
+def preprocess_and_tokenize(unprocessed_texts):
+    '''
+    :param unprocessed_texts: List of Reviews(String)
+    :return: Tokenized sequences with padding upto length 50
+    '''
+    processed = []
+    for review in unprocessed_texts:
+        processed.append(preprocess_text(review))
+    file = open("tokenizer.pkl", 'rb')
+    # tokenizer = tf.keras.preprocessing.text.Tokenizer()
+    tokenizer = pickle.load(file)
+    file.close()
+    tokenized = tokenizer.texts_to_sequences(processed)
+    tokenized_padded= tf.keras.preprocessing.sequence.pad_sequences(tokenized, padding='post', maxlen=maxlen)
+    return tokenized_padded
+
+if __name__ == '__main__':
+    # model = tf.keras.Sequential()
+    model = tf.keras.models.load_model('Models/c1_lstm_model_acc_highaccr0.827.keras')
+    #Add Reviews in input_reviews.csv file
+    input_reviews = pd.read_csv('input_reviews.csv')
+    # unseen_example1 = "as good as gta is, dont buy it." \
+    #                  " Dont support rockstar making dumb decisions," \
+    #                  " removing content, bringing back content from " \
+    #                  "years ago and branding it as a\" new update\"," \
+    #                  " game is full of hackers, your ip is literally " \
+    #                  "accesible to every 3 year old with a mod menu," \
+    #                  " plenty of bugs, rockstar does not give a single " \
+    #                  "♥♥♥♥ about its community. Whatever you say, rockstar" \
+    #                  " doesnt care and only does what its investors say."
+    # unseen_example2 = "Still the best game available."
+    list_inputs = input_reviews['Reviews']
+    preprocessed_inputs = preprocess_and_tokenize(list_inputs)
+    prediction = np.round(model.predict(preprocessed_inputs),2)
+    print(prediction)
